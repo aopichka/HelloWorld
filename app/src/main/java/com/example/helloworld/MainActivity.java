43a21a95bae73db44dc4a65c242b57cf3156eabd
aopichka <adam.opichka@gmail.com>
2020-01-31 14:57:21 -0600

    Added user's age to project

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int age;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
